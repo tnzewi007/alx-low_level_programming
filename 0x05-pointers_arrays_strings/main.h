1665ade16343b39b2f47f446368c6c9604951cff
tnzewi007 <tnzewi@gmail.com>
2022-09-20 18:00:54 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * void prototypes()
- * int prototypes()
- */
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /*MAIN_H*/
+#endif
