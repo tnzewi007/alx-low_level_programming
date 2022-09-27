49661cc77d4f9c90b456a1761f1df6dc2fc1a59d
tnzewi007 <tnzewi@gmail.com>
2022-09-27 15:14:53 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,17 @@
 #include "main.h"
-
 /**
- *_strcat - concatenates  the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will be appended
- *@src: String to be concatenated upon
- *
- * Return: returns poiner to @dest
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
-
-char *_strcat(char *dest, char *src)
+char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
-	int index = 0, dest_len = 0;
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	return (s);
 }
