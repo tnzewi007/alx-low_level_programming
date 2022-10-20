d3689be777c1f89e1577052658cbd7d53391e22d
tnzewi007 <tnzewi@gmail.com>
2022-10-20 16:21:46 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a linked list
+ * @head: The node at the Beginning
+ */
+void free_list(list_t *head)
+{
+if (head)
+{
+if (head->next)
+free_list(head->next);
+if (head->str)
+free(head->str);
+if (head)
+free(head);
+}
+}
