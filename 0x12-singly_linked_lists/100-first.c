b022a703019c6d47d14a8216c4f7f2808d9a67f8
tnzewi007 <tnzewi@gmail.com>
2022-10-20 16:25:39 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void first(void)__attribute__((constructor));
+
+/**
+ * first - check the code for Alx school students.
+ *
+ */
+
+void first(void)
+
+{
+printf("You're beat! and yet, you must allow,%s",
+"\nI bore my house upon my back!\n");
+
+}
